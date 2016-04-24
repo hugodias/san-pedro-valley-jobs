badedff74f193efa73013cc433feaa0fbf6ab2d1
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2016-04-24 12:07:48 -0300

    Using blocks to define random values to attributes other than titles

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,16 +1,14 @@
 FactoryGirl.define do
-  sequence(:title) { |n| "Example title #{n}" }
-
   factory :job do
-    title
-    salary "R$ #{Faker::Commerce.price}"
-    location Faker::Address.city
+    sequence(:title) { |n| "Job #{n}" }
+    salary { "R$ #{Faker::Commerce.price}" }
+    location { Faker::Address.city }
     description Faker::Lorem.paragraph(4)
     how_to_apply Faker::Lorem.paragraph(1)
     job_type
     category
     company
-    author Faker::Name.name
-    author_email Faker::Internet.email
+    author { Faker::Name.name }
+    author_email { Faker::Internet.email }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :user do
     name Faker::Name.name
-    email Faker::Internet.email
+    email { Faker::Internet.email }
     password Faker::Internet.password(10)
   end
 
   factory :admin, class: User do
     name Faker::Name.first_name
-    email Faker::Internet.email
+    email { Faker::Internet.email }
     password Faker::Internet.password(8)
     admin true
   end
