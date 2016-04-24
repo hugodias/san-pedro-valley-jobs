95facdc34cec55eef13e2c4526ce4b3296f0b00e
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2016-04-24 02:34:33 -0300

    Changed mail settings location and its env variable names

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,20 +27,6 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.mailgun.org",
-    port: 587,
-    domain: ENV['MAILGUN_DOMAIN'],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['MAILGUN_USERNAME'],
-    password: ENV['MAILGUN_PASSWORD']
-  }
-
-  config.action_mailer.default_url_options = {
-    host: 'localhost:3000'
-  }
-
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,18 +71,7 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.mailgun.org",
-    port: 587,
-    domain: ENV['MAILGUN_DOMAIN'],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['MAILGUN_USERNAME'],
-    password: ENV['MAILGUN_PASSWORD']
-  }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
 
