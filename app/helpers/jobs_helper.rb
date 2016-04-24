518626316259fe23975104fd414b197ae202a911
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2016-04-24 10:37:15 -0300

    icon size in icon_by_category helper should be optional and customizable

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,27 +1,27 @@
 module JobsHelper
 
-  def icon_by_category(category)
+  def icon_by_category(category, size='2x')
     case category
     when 'Desenvolvimento'
-      fa_icon 'code 2x'
+      fa_icon "code #{size}"
     when 'Design'
-      fa_icon 'paint-brush 2x'
+      fa_icon "paint-brush #{size}"
     when 'Marketing'
-      fa_icon 'line-chart 2x'
+      fa_icon "line-chart #{size}"
     when 'Social Media'
-      fa_icon 'share-square 2x'
+      fa_icon "share-square #{size}"
     when 'Business/Executivo'
-      fa_icon 'briefcase 2x'
+      fa_icon "briefcase #{size}"
     when 'Operações'
-      fa_icon 'gears 2x'
+      fa_icon "gears #{size}"
     when 'Relacionamento com o cliente'
-      fa_icon 'comments-o 2x'
+      fa_icon "comments-o #{size}"
     when 'Suporte'
-      fa_icon 'group 2x'
+      fa_icon "group #{size}"
     when 'Comercial'
-      fa_icon 'money'
+      fa_icon "money #{size}"
     when 'Diversos'
-      fa_icon 'asterisk'
+      fa_icon "asterisk #{size}"
     end
   end
 end
