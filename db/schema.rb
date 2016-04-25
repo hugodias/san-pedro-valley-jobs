3c7fdadc3f872c7ad3921335b53807bf5fd7fe3f
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2016-04-24 22:19:33 -0300

    Added external link field to job form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421134650) do
+ActiveRecord::Schema.define(version: 20160425003647) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20160421134650) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.string   "token"
+    t.string   "link"
   end
 
   add_index "jobs", ["category_id"], name: "index_jobs_on_category_id"
