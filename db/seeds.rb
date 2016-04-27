0b12028005b657373e9ca4b3fb628434d6cd2693
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2016-04-27 07:59:59 -0300

    Prevent create admin multiple times

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+if User.all.empty?
+  user = CreateAdminService.new.call
+  puts 'CREATED ADMIN USER: ' << user.email
+end
 
 Category.create([
   {title: 'Design'},
