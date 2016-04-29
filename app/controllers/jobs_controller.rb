0229c6139ec23662c145f2d4971144a04fe8274e
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2016-04-29 00:30:13 -0300

    Typo

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -31,7 +31,7 @@ class JobsController < ApplicationController
     @job = Job.new(job_params)
 
     if @job.save
-      flash[:notice] = 'Tudo pronto. A vaga ja foi salva, agora precisamos analisar e decidir se a mesma será publicada ou não. Você receberá um e-mail com a resposta em breve.'
+      flash[:notice] = 'Tudo pronto, a vaga foi salva com sucesso. Precisamos analisar e decidir se a mesma será publicada ou não no site. Você receberá um e-mail com a resposta em breve.'
 
       redirect_to root_url
     else
