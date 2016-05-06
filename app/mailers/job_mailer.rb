7774fc5051066d5c08660a9d97b9677159199a02
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2016-05-05 21:52:34 -0300

    Removing single quotes

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -1,19 +1,21 @@
 class JobMailer < ApplicationMailer
-  default from: ENV['DEFAULT_FROM']
-  layout 'jobmailer'
+  default from: ENV["DEFAULT_FROM"]
+  layout "jobmailer"
 
   def job_created(job)
     @job = job
-    mail(to: ENV['ADMIN_EMAIL'], subject: "[SPV Jobs] Revisar vaga: #{job.title}")
+    mail(
+      to: ENV["ADMIN_EMAIL"],
+      subject: "[SPV Jobs] Revisar vaga: #{job.title}")
   end
 
   def job_approved(job)
     @job = job
-    mail(to: job.author_email, subject: '[SPV Jobs] Sua vaga foi aprovada :)')
+    mail(to: job.author_email, subject: "[SPV Jobs] Sua vaga foi aprovada :)")
   end
 
   def job_reproved(job)
     @job = job
-    mail(to: job.author_email, subject: '[SPV Jobs] Sua vaga foi reprovada :/')
+    mail(to: job.author_email, subject: "[SPV Jobs] Sua vaga foi reprovada :/")
   end
 end
